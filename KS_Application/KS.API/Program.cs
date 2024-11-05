8fd445fd5a2bc32fb3a45db467071d1c052a4168
DESKTOP-NDCBFHM\MrUser <robert.kamenski.rld@gmail.com>
2024-11-05 08:49:23 +0100

    feat: Add swagger

--- KS_Application/KS.API/Program.cs
+++ KS_Application/KS.API/Program.cs
@@ -2,6 +2,7 @@ using FluentValidation.AspNetCore;
 using KS.API.Extensions;
 using KS.Application;
 using KS.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +12,23 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "KS_Application_API", Version = "v1" });
+});
+
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "KS_Application_API v1");
+    });
+}
 
 app.MapHealthChecks("/api/health");
 
